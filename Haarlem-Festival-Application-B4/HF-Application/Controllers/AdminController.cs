a996bdb491b1ceecd9d19629efed0b0b56d22d10
Daniël Kroese <572068@student.inholland.nl>
2017-11-21 13:34:01 +0100

    Test voor Ercan

---
+++ Haarlem-Festival-Application-B4/HF-Application/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HF_Application.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+		{
+			Console.WriteLine("Hoi Ercan!");
+			return View();
+        }
+    }
+}
