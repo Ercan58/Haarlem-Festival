02c29eb42a1c9dd61760e9a046db4dd4591782f5
user <user@DESKTOP-JIOVEJ7>
2017-11-21 15:55:14 +0100

    modellen zijn aangemaakt heb wat aanpassingen gedaan Event= Abstract en heb 2 enums aangemaakt

---
+++ Haarlem-Festival-Application-B4/HF-Application/Models/Enum/Hall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HF_Application.Models.Enum
+{
+    public enum Hall
+    {
+
+    }
+}
--- Haarlem-Festival-Application-B4/HF-Application/Models/Location.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Models/Location.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HF_Application.Models.Enum;
 
 namespace HF_Application.Models
 {
     public class Location
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Zipcode { get; set; }
+        public string Remarks { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+        public Hall Hall { get; set; }
     }
 }
