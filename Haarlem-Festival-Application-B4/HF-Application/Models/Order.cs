882709e9b2c6b494d5fc8f433ca91f37f1d90377
user <user@DESKTOP-JIOVEJ7>
2017-11-21 14:09:17 +0100

    property's zijn aangemaakt maar niet compleet

--- Haarlem-Festival-Application-B4/HF-Application/Models/Order.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Models/Order.cs
@@ -7,5 +7,10 @@ namespace HF_Application.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public int Invoice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime OrderPayed { get; set; }
+
     }  
 }
