6b36bb6e3a9264d605f3bdeabedf24755c736f56
user <user@DESKTOP-JIOVEJ7>
2017-12-04 13:45:34 +0100

    Testen of middels scaffolding insert kunnen uitvoeren

---
+++ Haarlem-Festival-Application-B4/HF-Application/Controllers/DinersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HF_Application.Models;
+using HF_Application.Models.Events;
+
+namespace HF_Application.Controllers
+{
+    public class DinersController : Controller
+    {
+        private HaarlemFestivalContext db = new HaarlemFestivalContext();
+
+        // GET: Diners
+        public ActionResult Index()
+        {
+            return View(db.Events.ToList());
+        }
+
+        // GET: Diners/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diner diner = (Diner)db.Events.Find(id);
+            if (diner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diner);
+        }
+
+        // GET: Diners/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Diners/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Location,CartDescription,CartTitle,Price,Seats,TicketType,EndDate,StartDate,Session,RestaurantName,imagePath,FoodType")] Diner diner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(diner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(diner);
+        }
+
+        // GET: Diners/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diner diner = (Diner)db.Events.Find(id);
+            if (diner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diner);
+        }
+
+        // POST: Diners/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Location,CartDescription,CartTitle,Price,Seats,TicketType,EndDate,StartDate,Session,RestaurantName,imagePath,FoodType")] Diner diner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(diner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(diner);
+        }
+
+        // GET: Diners/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diner diner = (Diner)db.Events.Find(id);
+            if (diner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diner);
+        }
+
+        // POST: Diners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Diner diner = (Diner)db.Events.Find(id);
+            db.Events.Remove(diner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Haarlem-Festival-Application-B4/HF-Application/Models/User.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Models/User.cs
@@ -8,6 +8,7 @@ namespace HF_Application.Models
     public class User
     {
         public int Id { get; set; }
+        public bool Admin { get; set; }
         public string Email { get; set; }
         public string Mail { get; set; }
         public string Password { get; set; }
