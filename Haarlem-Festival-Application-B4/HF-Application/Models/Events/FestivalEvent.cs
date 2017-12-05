899da80be4e2de13c609a9cfdd99dfea195c3955
user <user@DESKTOP-JIOVEJ7>
2017-12-05 15:45:27 +0100

    Daniel speciaal voor jou

--- Haarlem-Festival-Application-B4/HF-Application/Models/Events/FestivalEvent.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Models/Events/FestivalEvent.cs
@@ -13,8 +13,6 @@ namespace HF_Application.Models
         public int LocationId { get; set; }
         public string CartDescription { get; set; }
         public string CartTitle { get; set; }
-        public double Price { get; set; }
-        public int Seats { get; set; }
         public TicketType TicketType { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime StartDate { get; set; }
