d6149f601d93312259cf3276dd076765a2b118be
Shahin <smm87@live.nl>
2017-12-07 21:58:10 +0100

    12

--- Haarlem-Festival-Application-B4/HF-Application/Models/HaarlemFestivalContext.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Models/HaarlemFestivalContext.cs
@@ -34,6 +34,5 @@ namespace HF_Application.Models
         public System.Data.Entity.DbSet<HF_Application.Models.RestaurantFoodtype> RestaurantFoodtypes { get; set; }
         public System.Data.Entity.DbSet<HF_Application.Models.TourGuid> TourGuid { get; set; }
         public System.Data.Entity.DbSet<HF_Application.Models.TourLanguage> TourLanguage { get; set; }
-        public System.Data.Entity.DbSet<HF_Application.Models.TourGuideLang> TourGuideLang { get; set; }
     }
 }
