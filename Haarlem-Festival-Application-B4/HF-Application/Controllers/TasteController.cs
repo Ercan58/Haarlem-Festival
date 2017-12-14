11a3e3f7531238fbade4ece0dbd347d164a9e69a
Daniël Kroese <daniel@kroese.com>
2017-12-14 14:43:05 +0100

    Controllers hernoemd voor duidelijkere links, urls in de menubar gefixt.

---
+++ Haarlem-Festival-Application-B4/HF-Application/Controllers/TasteController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HF_Application.Models;
+using HF_Application.Models.Events;
+using HF_Application.Models.ViewModel;
+
+namespace HF_Application.Controllers
+{
+    public class TasteController : Controller
+    {
+        private HaarlemFestivalContext db = new HaarlemFestivalContext();
+
+        private List<FoodType> AllFoodTypes;
+        private List<Restaurant> AllRestaurants;
+        private List<FoodType> FirstTillSixFoodtypes;
+        private List<FoodType> sevenTillNineFoodtypes;
+
+        public TasteController()
+        {
+            AllRestaurants = new List<Restaurant>();
+            FirstTillSixFoodtypes = new List<FoodType>();
+            sevenTillNineFoodtypes = new List<FoodType>();
+
+
+            this.AllRestaurants = GetAllRestaurants();
+            this.AllFoodTypes = GetAllFoodtypes();
+            this.FirstTillSixFoodtypes = selectFoodTypes(0, 5);
+            this.sevenTillNineFoodtypes = selectFoodTypes(6, 8);
+        }
+
+        public List<Restaurant> GetAllRestaurants()
+        {
+            List<Restaurant> Restaurants = new List<Restaurant>();
+            return Restaurants = db.Restaurants.Include(a=>a.FoodTypes).Include(b=>b.Location).ToList();
+        }
+
+        public List<FoodType> GetAllFoodtypes()
+        {
+            List<FoodType> FoodTypes = db.Foodtypes.ToList();
+            return FoodTypes;
+        }
+
+        public List<FoodType> selectFoodTypes(int start, int end)
+        {
+            List<FoodType> foodtypes = new List<FoodType>();
+
+            for(int a=start; a<=end; a++)
+            {
+                foodtypes.Add(AllFoodTypes[a]);
+            }
+            return foodtypes;
+        }
+
+        public List<Restaurant> GetRestaurants(int id)
+        {
+            List<Restaurant> restaurants = new List<Restaurant>();
+
+            var foodtypes = db.RestaurantFoodtypes.Where(f => f.Foodtype.Id == id).Include(r=>r.Restaurant).Include(f=>f.Foodtype).ToList();
+
+            foreach(RestaurantFoodtype restaurantfoodtype in foodtypes)
+            {
+                restaurants.Add(restaurantfoodtype.Restaurant);
+            }
+
+            return restaurants;
+        }
+
+        // GET: Diners
+        public ActionResult Index(int? id)
+        {
+            RestaurantModel restaurantmodel = new RestaurantModel();
+            restaurantmodel.ListOfSixFoodtypes = FirstTillSixFoodtypes;
+            restaurantmodel.ListOfThreeFoodtypes = sevenTillNineFoodtypes;
+
+
+            if (id== null)
+            {
+                //Pakket samenvoegen zodat dit gebruiksklaar is...
+                restaurantmodel.Restaurants = AllRestaurants;
+            }
+            else
+            {
+                int categorieId = id.GetValueOrDefault();   
+                restaurantmodel.Restaurants = GetRestaurants(categorieId);
+                
+            }
+            return View(restaurantmodel);   
+        }
+
+    }
+}
