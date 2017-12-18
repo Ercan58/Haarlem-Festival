772365dc0d5d9391c80e3b7cd9f64f9f591aedec
Shahin <smm87@live.nl>
2017-12-18 14:01:08 +0100

    Jazz

---
+++ Haarlem-Festival-Application-B4/HF-Application/Repositories/JazzRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HF_Application.Models;
+using HF_Application.Models.Events;
+using HF_Application.Models.ViewModel;
+
+namespace HF_Application.Repositories
+{
+    public class JazzRepository : IJazzRepository
+    {
+
+        private HaarlemFestivalContext db = new HaarlemFestivalContext();
+        public List<Jazz> GetAllJazzEvents()
+        {
+            List<Jazz> JazzEvents = new List<Jazz>();
+            return JazzEvents = db.Jazzs.Include(a => a.Location).ToList();
+        }
+
+        //public List<Jazz> GetSelectionFilter()
+        //{
+        //    List<Jazz> JazzEventsDate = new List<Jazz>();
+        //    //List<DateTime> JazzEventsStartDate = new List<DateTime>();
+        //    foreach (Jazz j in db.Jazzs)
+        //        if (JazzEventsStartDate.Contains(j.StartDate))
+        //        { }
+        //        else
+        //        {
+        //            JazzEventsStartDate.Add(j.StartDate);
+        //        }
+
+        //    return JazzEventsDate;
+        //}
+    }
+}
