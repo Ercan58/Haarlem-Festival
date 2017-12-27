a1ee6c70fb943dbf1a6a4addc2d2e60af50c7317
Adham al Sabsabi <585693@student.inholland.nl>
2017-12-27 15:12:39 +0100

    TalkRepository-Desigs css

---
+++ Haarlem-Festival-Application-B4/HF-Application/Interface/ITalkRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HF_Application.Models.Events;
+
+namespace HF_Application.Interface
+{
+    interface ITalkRepository
+    {
+        List<Talk> GetAllTalkEvents();
+        List<Talk> GetTalkEvents(DateTime date);
+        List<Talk> GetCurrentTalkEvent(int id);
+        List<Talk> GetCrossTalkEvents(int id);
+
+    }
+}
---
+++ Haarlem-Festival-Application-B4/HF-Application/Repositories/TalkRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HF_Application.Interface;
+using HF_Application.Controllers;
+using HF_Application.Models;
+using HF_Application.Models.Events;
+using HF_Application.Models.ViewModel;
+using System.Data;
+using System.Data.Entity;
+
+
+namespace HF_Application.Repositories
+{
+    public class TalkRepository : ITalkRepository
+    {
+        private HaarlemFestivalContext db = new HaarlemFestivalContext();
+
+        public List<Talk> GetAllTalkEvents()
+        {
+            List<Talk> TalkEvents = new List<Talk>();
+            return TalkEvents = db.Talks.Include(a => a.Location).ToList();
+        }
+
+        public List<Talk> GetCrossTalkEvents(int id)
+        {
+            List<Talk> talkEvent = new List<Talk>();
+            var selectionById = db.Talks.Where(d => d.ID != id).Include(l => l.Location).ToList();
+            foreach (Talk TalkEvent in selectionById)
+            {
+                talkEvent.Add(TalkEvent);
+            }
+            return talkEvent;
+        }
+
+        public List<Talk> GetCurrentTalkEvent(int id)
+        {
+            List<Talk> talkEvent = new List<Talk>();
+            var selectionById = db.Talks.Where(d => d.ID == id).Include(l => l.Location).ToList();
+            foreach (Talk TalkEvent in selectionById)
+            {
+                talkEvent.Add(TalkEvent);
+            }
+            return talkEvent;
+        }
+
+        public List<Talk> GetTalkEvents(DateTime date)
+        {
+            List<Talk> talkEvents = new List<Talk>();
+
+            var selectionByStartDate = db.Talks.Where(d => d.StartDate == date).Include(l => l.Location).ToList();
+            foreach (Talk TalkEvent in selectionByStartDate)
+            {
+                talkEvents.Add(TalkEvent);
+            }
+            return talkEvents;
+        }
+    }
+}
