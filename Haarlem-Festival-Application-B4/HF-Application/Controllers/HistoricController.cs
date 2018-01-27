d1abaa7d0ac5e4519a472462bb55bd3665862d88
AdhamSab <585693@student.inholland.nl>
2018-01-27 22:20:00 +0100

    aaaaaaaaaa

--- Haarlem-Festival-Application-B4/HF-Application/Controllers/HistoricController.cs
+++ Haarlem-Festival-Application-B4/HF-Application/Controllers/HistoricController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Mvc;
 using HF_Application.Models;
 using HF_Application.Models.Events;
 using HF_Application.Models.ViewModel;
