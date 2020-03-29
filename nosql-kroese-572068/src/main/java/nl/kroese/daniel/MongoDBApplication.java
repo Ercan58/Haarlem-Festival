e5a43a67415eb9b687ba835163304b98d6af658d
danielkroese <daniel@kroese.com>
2020-03-29 15:28:54 +0200

    Some things

--- nosql-kroese-572068/src/main/java/nl/kroese/daniel/MongoDBApplication.java
+++ nosql-kroese-572068/src/main/java/nl/kroese/daniel/MongoDBApplication.java
@@ -1,61 +1,52 @@
 package nl.kroese.daniel;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Collation;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.bson.Document;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class MongoDBApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Bar Chart Sample");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String, Number> chart =
-                new BarChart<>(xAxis, yAxis);
+        primaryStage.setTitle("Afgestudeerden Haarlem");
+        VBox group = new VBox();
+        group.setSpacing(1.0);
+
         MongoDatabase database = Database.getDatabase();
         MongoCollection<Document> collection = database.getCollection("hbo_gediplomeerden_2019");
-        FindIterable<Document> iterable = collection.find();
-        MongoCursor<Document> cursor = iterable.iterator();
 
-        // Get the first object from the cursor
-        Document obj = cursor.next();
+        FindIterable<Document> iterable = collection
+                .find(eq("GEMEENTENAAM", "Haarlem"))
+                .sort(new BasicDBObject("2018", -1))
+                .limit(5);
+        for (Document document : iterable) {
+            List<Map.Entry<String, Object>> set = new ArrayList<>(document.entrySet());
 
-        // Get all the dates from the entry set but exclude the first 5 (_id, province, country, lat, long)
-        List<Map.Entry<String, Object>> set = obj.entrySet().stream().skip(5).collect(Collectors.toList());
-
-        // Create a chart series to fill with your data
-        Series series = new Series();
-
-        // Loop trough the date's and add them to the series
-        for (Map.Entry<String, Object> entry : set) {
-            series.getData().add(new XYChart.Data<>(entry.getKey(), Integer.parseInt(entry.getValue().toString())));
+            Label label = new Label(set.get(9).getValue().toString() + " geslaagd: " + set.get(17).getValue().toString());
+            group.getChildren().add(label);
         }
 
-        // Append the series to the chart
-        chart.getData().add(series);
-        chart.setTitle("Confirmed Corona Cases");
-        xAxis.setLabel("Date");
-        yAxis.setLabel("Cases");
-
-        Scene scene = new Scene(chart, 500, 400);
+        Scene scene = new Scene(group, 500, 400);
         primaryStage.setScene(scene);
 
         primaryStage.show();
@@ -65,7 +56,7 @@ public class MongoDBApplication extends Application {
         MongoDBApplication.launch(args);
     }
 
-    public void loadCSV(){
+    public void loadCSV() {
         MongoDatabase database = Database.getDatabase();
         String csvFile = "/Users/danielkroese/Desktop/05-gediplomeerden-hbo-2019.csv";
         BufferedReader reader = null;
@@ -83,8 +74,17 @@ public class MongoDBApplication extends Application {
                 String[] documentLine = line.split(splitChar);
 
                 Document document = new Document();
-                for (int i=1; i < columns.length; i++){
-                    document.append(columns[i], documentLine[i]);
+                for (int i = 1; i < columns.length; i++) {
+
+                    // parse as ints for graduated amount
+                    if (columns[i].equals("2018") ||
+                            columns[i].equals("2017") ||
+                            columns[i].equals("2016") ||
+                            columns[i].equals("2015") ||
+                            columns[i].equals("2014")) {
+                        document.append(columns[i], Integer.parseInt(documentLine[i]));
+                    } else
+                        document.append(columns[i], documentLine[i]);
                 }
                 MongoCollection<Document> collection = database.getCollection("hbo_gediplomeerden_2019");
                 collection.insertOne(document);
